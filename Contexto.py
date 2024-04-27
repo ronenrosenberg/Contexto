aeee95ba74c3886a4ace1da8e7eafe83fdd7de8d
Knovnov <notknovus@gmail.com>
2024-04-27 01:38:44 -0700

    I hate all of this code thanks

--- Contexto.py
+++ Contexto.py
@@ -5,40 +5,53 @@ import gensim.downloader as api
 from gensim.models import KeyedVectors
 import os
 
+import json
+import random
+
+with open('quotes.json', 'r') as file:
+    # Load the JSON data from the file into a Python dictionary
+    quotes = json.load(file)
+
 # Check if the model file exists
-model_file = 'fasttext-wiki-news-subwords-300.model'
+model_file = 'word2vec-google-news-300.model'
 if os.path.isfile(model_file):
-    print("a")
     # Load the model from disk
     vectorized_data = KeyedVectors.load(model_file)
 else:
-    print("b")
     # Load the model from the API if the file doesn't exist
-    vectorized_data = api.load('fasttext-wiki-news-subwords-300')
+    vectorized_data = api.load('word2vec-google-news-300')
     # Save the model to disk for future use
     vectorized_data.save(model_file)
 
 
-example_sentence = "Thing.".replace(".","").replace(",","").replace("!","").replace("?","").split()
+while True:
+    print(f"What kind of quote would you like out of the following? {list(quotes.keys())}")
+    category = input()
+    if category in list(quotes.keys()):
+        selected = random.choice(quotes[category]).replace(".","").replace(",","").replace("!","").replace("?","").split()
+        break
+    print("Sorry, what?")
+    print()
 
-tagged_sentence = nltk.pos_tag(example_sentence)
-#
+tagged_sentence = nltk.pos_tag(selected)
+
 important_words = []
 #for all words in the sentence
 for i in range(len(tagged_sentence)):
     #if word is a verb or nounj
     to_be_guessed = False
+    similar_list = []
     if tagged_sentence[i][1][0] in ["V", "N"]:
         #create a list of similar words
-        similar_list = vectorized_data.most_similar(tagged_sentence[i][0], topn=20)
+        similar_list = vectorized_data.most_similar(tagged_sentence[i][0], topn=10)
         #make sure words in similar list are all lowercase and of same part of speech
-        similar_list = [data[0].lower() for data in similar_list if nltk.pos_tag([data[0]])[0][1] == tagged_sentence[i][1] and tagged_sentence[i][0] != data[0].lower()]
+        similar_list = [data[0].lower().replace(".","").replace(",","").replace("!","").replace("?","") for data in similar_list if nltk.pos_tag([data[0]])[0][1] == tagged_sentence[i][1] and tagged_sentence[i][0] != data[0].lower()]
         
+        print(similar_list)
         to_be_guessed = True
         #index to replace, original word, and similar word list
     important_words.append([to_be_guessed, [tagged_sentence[i][0].lower()] + similar_list])
 
-print(important_words)
 
 def print_and_underline():
     print_list = []
@@ -52,9 +65,9 @@ def print_and_underline():
         #else just print it
         else:
             if i == 0:  
-                print_list.append(example_sentence[i].capitalize().replace("_", " "))
+                print_list.append(important_words[i][1][-1].capitalize().replace("_", " "))
             else:
-                print_list.append(example_sentence[i].replace("_", " "))
+                print_list.append(important_words[i][1][-1].replace("_", " "))
 
     #actual printing
     for i in range(len(print_list)):
@@ -75,11 +88,12 @@ def check_if_better(user_guess):
                     print(f"You got it! \"{user_guess}\" was one of the missing words.")
                 else:
                     new_index = word[1][:-1].index(user_guess)
-                    word[2] = word[1][:new_index+1]
+                    word[1] = word[1][:new_index+1]
                     print(f"Nice! \"{user_guess}\" is {new_index} degrees of separation away from the actual word.")
     if not changed:
         print("Sorry, but that doesn't seem to be right.")
     print()
+
 
 while True:
     print_and_underline()
